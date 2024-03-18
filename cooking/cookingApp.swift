dd24955a9b334d2526fabd104da59a3078d69686
Yala Huang <yalahuangfeng@gmail.com>
2024-03-17 22:43:50 -0400

    Initial Commit

---
+++ cooking/cookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookingApp.swift
+//  cooking
+//
+//  Created by Teacher on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct cookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
