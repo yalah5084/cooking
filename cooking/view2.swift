98b3a72e22d25e4aac5e74280baa04c93e3fb966
Yala Huang <yalahuangfeng@gmail.com>
2024-04-05 13:17:00 -0400

    second file to tinker

---
+++ cooking/view2.swift
@@ -0,0 +1,18 @@
+//
+//  view2.swift
+//  cooking
+//
+//  Created by Teacher on 4/5/24.
+//
+
+import SwiftUI
+
+struct view2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    view2()
+}
