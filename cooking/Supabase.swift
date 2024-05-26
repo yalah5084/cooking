e7fb9e90d0fa24915d4e1704f4bc0872e46e78a3
Hanna Li <hannal7626@hstat.org>
2024-05-26 19:47:03 -0400

    add supabase

---
+++ cooking/Supabase.swift
@@ -0,0 +1,14 @@
+//
+//  Supabase.swift
+//  cooking
+//
+//  Created by hanna li on 5/26/24.
+//
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://zequqetcpsmeltoanvad.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplcXVxZXRjcHNtZWx0b2FudmFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY3NjU3MjYsImV4cCI6MjAzMjM0MTcyNn0.2ryIpLOVOBLDG3zFXqFX3NT-MIYHwqc58VcAJaJeYt4"
+)
